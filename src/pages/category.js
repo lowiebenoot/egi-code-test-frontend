a4d4ad2368fcf81796370feefafb5c7cecd549eb
Lowie Benoot <lowie@teamleader.be>
2019-11-28 12:23:25 +0800

    Wrap the app with the apollo provider so we can query with graphql

--- src/pages/category.js
+++ src/pages/category.js
@@ -1,13 +1,17 @@
 import React from "react"
 import { Router } from "@reach/router"
+import { ApolloProvider } from "@apollo/react-hooks"
 
+import client from "../apollo"
 import Characters from "../components/clientSidePages/characters"
 
 const App = () => {
   return (
-    <Router>
-      <Characters path="/category/characters" />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Characters path="/category/characters" />
+      </Router>
+    </ApolloProvider>
   )
 }
 export default App
