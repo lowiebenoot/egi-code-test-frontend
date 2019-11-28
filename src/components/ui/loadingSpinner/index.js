9b659384a77d141697afacca7f471a1c684235a4
Lowie Benoot <lowie@teamleader.be>
2019-11-28 16:58:01 +0800

    created a loading spinner component

---
+++ src/components/ui/loadingSpinner/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import styles from "./styles.module.css"
+
+const LoadingSpinner = () => {
+  return <div className={styles.loadingSpinner} />
+}
+
+export default LoadingSpinner
