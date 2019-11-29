94ea0c83f95d725de18d381f9621603f9079de31
Lowie Benoot <lowie@teamleader.be>
2019-11-29 14:44:42 +0800

    add some margin to the intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 import Button from "../components/ui/button"
+import styles from "./index.module.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>A song of GraphQL and Gatsby</h1>
-    <p>
+    <p className={styles.intro}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mollis
       congue suscipit. Integer gravida iaculis sapien in porta. Pellentesque
       ornare diam non urna tincidunt euismod. Cras mi felis, fringilla ut
