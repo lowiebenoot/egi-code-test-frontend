f82a71265a0059ccae0fd82cb895377bdb5f17fc
Lowie Benoot <lowie@teamleader.be>
2019-11-29 14:01:59 +0800

    use gatsby-s3-plugin to deploy to s3

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,12 @@ module.exports = {
         prefixes: [`/category/*`],
       },
     },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "egi-code-test-frontend",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
